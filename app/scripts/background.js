c229e42d1d30764f0b72b83c31a859cbb4b91aab
Johannes Edelstam <johannes@edelst.am>
2014-02-11 10:49:02 +0100

    Fix tab closing

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,12 +1,30 @@
 'use strict';
 
+var Pinner = {
+  closeUnpinned: function() {
+    chrome.tabs.query({lastFocusedWindow: true, pinned: false}, function(unpinned) {
+      var ids = unpinned.map(function(tab){
+        return tab.id
+      })
+      chrome.tabs.remove(ids);
+    });
+  },
+  togglePinned: function () {
+  
+    chrome.tabs.query({lastFocusedWindow: true, active: true}, function(active){
+      var current = active[0];
+      chrome.tabs.update(current.id, {pinned: !current.pinned});
+    });
+  }
+}
+
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
 chrome.commands.onCommand.addListener(function(command) {
-  chrome.tabs.query({lastFocusedWindow: true, active: true}, function(active){
-    var current = active[0];
-    chrome.tabs.update(current.id, {pinned: !current.pinned});
-  });
+  if(command == "toggle-pinned")
+    Pinner.togglePinned();
+  else if(command == "close-unpinned")
+    Pinner.closeUnpinned();
 });
